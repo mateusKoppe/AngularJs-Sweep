a08fcc07f6c00a1fed25c02d3fce5e800828ea77
Koppe <mateus.koppe@gmail.com>
2017-02-28 19:55:14 -0300

    Database change

--- database.sql
+++ database.sql
@@ -3,14 +3,14 @@ CREATE DATABASE sweep;
 USE sweep;
 
 CREATE TABLE studants (
-  studant_id int(11) NOT NULL,
+  studant_id int(11) NOT NULL AUTO_INCREMENT,
   studant_name varchar(40) NOT NULL,
   studant_times int(11) DEFAULT 0,
   user_id int(11) NOT NULL
 );
 
 CREATE TABLE users (
-  user_id int(11) NOT NULL,
+  user_id int(11) NOT NULL AUTO_INCREMENT,
   user_username varchar(50) NOT NULL,
   user_password varchar(20) NOT NULL,
   user_class varchar(60) DEFAULT NULL
