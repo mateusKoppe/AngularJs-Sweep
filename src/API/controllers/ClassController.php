e80a060444001eb0208f6c06296283250e6d3915
Mateus Koppe <mateus.koppe@gmail.com>
2017-12-08 01:18:36 -0200

    refactor(API): controllers OO

--- src/API/controllers/ClassController.php
+++ src/API/controllers/ClassController.php
@@ -1,18 +1,18 @@
 <?php
-
-    require_once 'database.php';
-    require_once 'functions.php';
-
     $request_body = file_get_contents('php://input');
     $data = json_decode($request_body);
 
-    if($data->action === 'create'){
-        $username = filterValue($data->className);
-        $sqlSelect = "SELECT user_id from users WHERE user_username = '$username' and user_password='$password'";
-        if(!$conn->query($sqlSelect)->rowCount()){
-            $sqlInsert = "INSERT INTO user (user_username, user_password) VALUES ('$username','$password')";
-            echo $conn->exec($sqlInsert);
-        }else{
-            echo "login_existente";
+    class ClassController()
+    {
+        public function create()
+        {
+            $username = filterValue($data->className);
+            $sqlSelect = "SELECT user_id from users WHERE user_username = '$username' and user_password='$password'";
+            if(!$conn->query($sqlSelect)->rowCount()){
+              $sqlInsert = "INSERT INTO user (user_username, user_password) VALUES ('$username','$password')";
+              echo $conn->exec($sqlInsert);
+            }else{
+              echo "login_existente";
+            }
         }
     }
