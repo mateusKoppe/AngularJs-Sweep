aee49f0d03fadb5350b84bdf0b9cb427f40589a5
Mateus Koppe <mateus.koppe@gmail.com>
2017-12-08 00:39:17 -0200

    refactor(API): start MVC structure

--- src/API/functions.php
+++ src/API/functions.php
@@ -1,6 +1,4 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
-	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
     function filterValue($value){
         return addslashes(!empty($value)?$value:false);
     }
