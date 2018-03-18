8abf323abc3bf01c369e78dd14a30c8925656948
Mateus Koppe <mateus.koppe@gmail.com>
2018-03-18 19:06:19 -0300

    refactor(class): redirect after response

--- app/class/first-time-controller.js
+++ app/class/first-time-controller.js
@@ -8,18 +8,21 @@
     function FirstTimeController($state, loginFactory, classFactory) {
         var vm = this;
         vm.setClass = setClass;
+        vm.class = {};
 
         vm.$onInit = function(){
             classFactory.getClassByUser(loginFactory.getUser())
-                .then(response => {
+                .then(function(response) {
                     vm.class = response.data;
                 });
         }
 
         function setClass(className) {
             vm.class.class_name = className;
-            classFactory.updateClass(vm.class);
-            $state.go('class');
+            classFactory.updateClass(vm.class)
+                .then(function() {
+                    $state.go('class');
+                });
         }
     }
 })();
