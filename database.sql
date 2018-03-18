b2492b8c8033b67adf8318e33783a8ffb014efeb
Mateus Koppe <mateus.koppe@gmail.com>
2018-03-18 00:43:28 -0300

    fix(database): fix sql errors

--- database.sql
+++ database.sql
@@ -1,15 +1,13 @@
-CREATE DATABASE sweep IF NOT EXISTS;
-
 USE sweep;
 
-CREATE TABLE users IF NOT EXISTS (
+CREATE TABLE users (
   user_id int(11) PRIMARY KEY AUTO_INCREMENT,
   user_username varchar(50) UNIQUE NOT NULL,
   user_password varchar(20) NOT NULL,
   user_class varchar(60) DEFAULT NULL
 );
 
-CREATE TABLE studants IF NOT EXISTS (
+CREATE TABLE studants (
   studant_id int(11) PRIMARY KEY AUTO_INCREMENT,
   studant_name varchar(40) NOT NULL,
   studant_times int(11) DEFAULT 0,
